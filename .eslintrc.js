31f0518f260c3ae5236083529cefffd0b5be293f
jay.wang <jay.wang@ef.com>
2017-12-20 16:42:08 +0800

    add configure file

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": ["eslint-config-airbnb"],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "ecmaVersion": 6,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "eslint-plugin-jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        // "linebreak-style": [
+        //     "error",
+        //     "unix"
+        // ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
